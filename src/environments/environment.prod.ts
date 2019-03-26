9bc8afd87da92f3cfd2a025a78b6c6b7ca046f8d
Olivier Nishimwe <olinx01@gmail.com>
2019-03-26 20:40:36 +0200

    Getting Started

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZ_xt6fdCNrkpZjOGgIS2Y4-HhAmYW0wk",
+    authDomain: "oshop-77127.firebaseapp.com",
+    databaseURL: "https://oshop-77127.firebaseio.com",
+    projectId: "oshop-77127",
+    storageBucket: "oshop-77127.appspot.com",
+    messagingSenderId: "223230560384"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZ_xt6fdCNrkpZjOGgIS2Y4-HhAmYW0wk",
+    authDomain: "oshop-77127.firebaseapp.com",
+    databaseURL: "https://oshop-77127.firebaseio.com",
+    projectId: "oshop-77127",
+    storageBucket: "oshop-77127.appspot.com",
+    messagingSenderId: "223230560384"
+  }
 };
 
 /*
