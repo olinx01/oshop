560d535ebe25514405ce6ff0cb59f84d1c44acac
Olivier Nishimwe <olinx01@gmail.com>
2019-04-03 10:46:33 +0200

    Authentication and Authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -41,7 +44,11 @@ import { LoginComponent } from './login/login.component';
     AngularFireAuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { AppUser } from '../models/app-user';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.scss']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  appUser: AppUser;
 
-  ngOnInit() {
+  constructor(public auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
 }
